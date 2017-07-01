6df3a0a1cb58d506570458a4bfafc862b46a0e65
ltaylor510 <ltaylor.jazz@gmail.com>
2017-06-30 18:10:00 -0700

    trying to get this working
    none

---
+++ ArrayTest.java
@@ -0,0 +1,17 @@
+package com.lttest.git.maven.testclasses;
+
+public class ArrayTest {
+
+	public static void main(String[] args) {
+		
+		int [] nums = {7712,29,34,500078};
+	
+		for (int i =0;i <= nums.length -1; i++){
+			System.out.println("mynums:"+ nums[i]);
+		}
+			
+	
+
+	}
+
+}
